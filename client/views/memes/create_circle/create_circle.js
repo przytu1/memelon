cb9672b72fd788def96fb8dfb742c3cf802f3cff
Pawel Przytula <pawel@appsilon.pl>
2014-10-25 18:23:17 +0200

    Saving new circle to the database and redirection to circle view

--- client/views/memes/create_circle/create_circle.js
+++ client/views/memes/create_circle/create_circle.js
@@ -10,6 +10,21 @@ Template.CreateCircle.events({
       circleEmails.push(friendEmail)
       Session.set("friends-in-circle", circleEmails)
       template.find('#friend-email').value = "";
+  },
+  'click #save-circle': function(event, template) {
+      var circleName = template.find('#circle-name').value;
+      var creatorEmail = Meteor.user().emails[0].address;
+      var circleEmails = 
+        Session.get("friends-in-circle") == undefined ? [] : Session.get("friends-in-circle");
+      circleEmails.push(creatorEmail);
+      var doc = {
+        owner: Meteor.userId(),
+        name: circleName,
+        users: circleEmails
+      }
+      Circles.insert(doc);
+      var createdCircle = Circles.find(doc).fetch()[0];
+      Router.go("circle.memes", {_id: createdCircle._id});
   }
 });
 
